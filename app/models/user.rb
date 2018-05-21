ac59e115a5e8efbdc05551d7da1e1717f103edbe
YouDongKwan <dkyou7@naver.com>
2018-05-21 08:38:04 +0000

    role playing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
+  rolify
+  include Authority::UserAbilities
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  after_create :set_default_role
+  
+  private
+  
+  def set_default_role
+    add_role :user
+  end
+  
 end
